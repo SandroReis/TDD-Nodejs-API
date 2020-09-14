45ce5c9091e16eb54920a708a1ebbbe52004f5c7
Sandro Reis <sandrouh@hotmail.com>
2020-09-14 20:03:17 -0300

    test: ensure emailValidator should be called with right parameters

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -18,4 +18,10 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
+  test('should emailValidator called with right parameters', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isValid).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
