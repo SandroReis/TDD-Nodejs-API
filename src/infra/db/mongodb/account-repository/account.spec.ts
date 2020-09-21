89353ccb82fa1814d6b365070b5f41445e78631f
Sandro Reis <sandrouh@hotmail.com>
2020-09-21 09:14:08 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -7,18 +8,17 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
-  test('Should return an account on success', () => {
-    expect(true).toBe(true)
-  //   const sut = new AccountMongoRepository()
-  //   const account = sut.add({
-  //     name: 'any_name',
-  //     email: 'any_email@email.com',
-  //     password: 'any_password'
-  //   })
-  //   expect(account).toBeTruthy()
-  //   expect(account.id).toBeTruthy()
-  //   expect(account.name).toBe('any_name')
-  //   expect(account.email).toBe('any_email@email.com')
-  //   expect(account.password).toBe('any_passwordany_password')
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@email.com')
+    expect(account.password).toBe('any_password')
   })
 })
