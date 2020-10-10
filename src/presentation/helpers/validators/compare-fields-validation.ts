b413fd9483f1b0781068372e8f81f969855c0e3e
Sandro Reis <sandrouh@hotmail.com>
2020-10-09 21:32:17 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -12,7 +12,7 @@ constructor (fieldName: string, fieldToCompareName: string) {
 
 validate (input: any): Error {
   if (input[this.fieldName] !== input[this.fieldToCompareName]) {
-    return new InvalidParamError(this.fieldName)
+    return new InvalidParamError(this.fieldToCompareName)
   }
 }
 }
